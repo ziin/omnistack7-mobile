99842a9511006089420d9652efd96dc490351a1a
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2019-06-17 16:50:35 -0400

    add on Feed like functionality

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -43,6 +43,10 @@ export default class Feed extends Component {
     });
   }
 
+  handleLike = async id => {
+    await api.post(`/posts/${id}/like`);
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -67,7 +71,10 @@ export default class Feed extends Component {
 
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <TouchableOpacity
+                    style={styles.action}
+                    onPress={() => this.handleLike(item._id)}
+                  >
                     <Image source={like} />
                   </TouchableOpacity>
                   <TouchableOpacity style={styles.action} onPress={() => {}}>
