625e039f143c29074306f805c4841c323eff2235
Alisson Schwanz <alissonludtkeschwanz@gmail.com>
2019-06-17 14:24:33 -0400

    add navigation on Feed page

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+import camera from "../assets/camera.png";
 
 export default class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity
+        style={{
+          marginRight: 10,
+          padding: 5,
+          paddingHorizontal: 10
+        }}
+        onPress={() => navigation.navigate("New")}
+      >
+        <Image source={camera} />
+      </TouchableOpacity>
+    )
+  });
+
   render() {
     return (
       <View>
